50173e9f21f792f4dd738c2c0cf2fe6bd23a73c2
grlaura <lauraribeiro.g@gmail.com>
2024-05-07 16:46:37 +0300

    Update: generated data

--- src/saveJson.m
+++ src/saveJson.m
@@ -1,16 +1,16 @@
-function save2DArrayToJson(array, filename)
+function saveJson(data, filename)
     % Save a 2D array to a JSON file with the specified filename.
     % Input:
-    %   - array: A 2D array (e.g., GTPointCoordinates)
+    %   - data: Struct
     %   - filename: Name of the output JSON file (e.g., 'output.json')
-
-    % Create a struct with the desired field name
-    data.GTPointCoordinates = array;
+    
 
     % Convert the struct to a JSON-formatted string
     jsonStr = jsonencode(data);
 
     % Write the JSON string to the specified file
+
+
     fid = fopen(filename, 'w');
     fprintf(fid, '%s', jsonStr);
     fclose(fid);
